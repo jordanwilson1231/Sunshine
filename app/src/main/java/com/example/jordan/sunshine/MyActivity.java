dfe0e3517ede77b30165ca1dcb3afc25f129f6a6
jordanwilson1231 <jordanwilson1231@gmail.com>
2014-07-27 22:52:00 -0700

    Sunshine 1.0 Functional

---
+++ app/src/main/java/com/example/jordan/sunshine/MyActivity.java
@@ -0,0 +1,21 @@
+package com.example.jordan.sunshine;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class MyActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new ForecastFragment())
+                    .commit();
+        }
+    }
+
+}
+
